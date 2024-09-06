79a7ea90e2cba8eef7e83ee469a4eae81988d834
HP <HP@DESKTOP-8E0FCDH>
2024-09-06 21:21:16 +0600

    nestedd.js

---
+++ nestedd.js
@@ -0,0 +1,27 @@
+/*** question
+if you get more then 80 then inside your friend score. 
+    If your friend get more than 80. then go for a lunch. 
+    if your friend get below 80 but greater than or equal 60 then tell your friend, good luck next time. 
+    if your friend get less than 60 but more than or equal to 40 then, keep your friend's message unseen.
+    if your friend get less than 40, block your friend
+if you get less than 80 go to home and sleep and act sad
+***/
+// solve
+const yourScore = 12;
+const friendScore = 85;
+
+if (yourScore > 80) {
+  console.log("Inside your friend score");
+
+  if (friendScore > 80) {
+    console.log("Go for a lunch with your friend.");
+  } else if (friendScore >= 60 && friendScore < 80) {
+    console.log("Tell your friend, good luck for next time.");
+  } else if (friendScore >= 40 && friendScore < 60) {
+    console.log("Keep your friend's message unseen.");
+  } else if (friendScore < 40) {
+    console.log("Block your friend.");
+  }
+} else {
+  console.log("Go home, sleep, and act sad.");
+}
